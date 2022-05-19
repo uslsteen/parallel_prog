527d862436b0ed165b99c0abadb43e2f53ac88b8
uslsteen <shurygin0171@gmail.com>
2022-05-19 07:48:16 +0300

    [lab1] add new templ scheme

--- MPI/lab1/lab1.cc
+++ MPI/lab1/lab1.cc
@@ -7,8 +7,6 @@
 
 #include <mpich/mpi.h>
 
-
-
 const double PI = std::atan(1) * 4;
 constexpr double T = 1,
                  X = 1;
@@ -20,16 +18,16 @@ constexpr double h = 1e-2,
 //! NOTE: num of steps
 //! NOTE: check correctness of rounding
 constexpr uint32_t x_steps = static_cast<uint32_t>(X / h),
-                   t_steps = static_cast<uint32_t>(T / tau);
+                   t_steps = static_cast<uint32_t>(T / tau); 
 
 inline double f(double x, double t) {
     return x + t;
 }
 
+
 std::ostream& operator <<(std::ostream &os, const std::vector<int>& vec) {
      for (auto && it : vec)
         os << it << std::endl;
-
     return os;
 }
 
@@ -51,8 +49,9 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     
-    if (commsize == 1)
+    if (commsize == 1)  
         do_linear(data);
+        //std::cout << end - start << std::endl;
     else
         do_parallel(data, rank, commsize);
 
@@ -61,23 +60,36 @@ int main(int argc, char** argv) {
 }
 
 void do_parallel(std::vector<std::vector<double>>& data, int32_t rank, int32_t commsize) {
+    
+    auto start = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
 
     do_main_job(data, rank, commsize);
     std::vector<double> result = do_last_part(data, rank, commsize);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    auto end = MPI_Wtime();
+
     result.resize(x_steps * t_steps);
 
-    if (rank == 0)
+    if (rank == 0) {
+        std::cout << (end - start) << " " << commsize << std::endl;
         dump(result, std::string{"data_parallel.out"});
+    }
 }
 
 void do_main_job(std::vector<std::vector<double>>& data, int32_t rank, int32_t commsize) {
     for (int32_t t = rank; t < t_steps; t += commsize) {
-        for (int32_t x = 0; x < x_steps - 1; ++x) {
+        for (int32_t x = 1; x < x_steps; ++x) {
             if (t != 0) {
                 int src = rank ? rank - 1 : commsize - 1;
+                
                 MPI_Recv(&(data[t - 1][x]), 1, MPI_DOUBLE, src, x, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 
-                data[t][x + 1] =  data[t][x] - (tau / h) * (data[t][x] - data[t-1][x]) + tau * f(x * h, t * tau);    
+                if (x == x_steps - 1)
+                    data[t][x] =  data[t-1][x] - (tau / h) * (data[t-1][x] - data[t-1][x - 1]) + tau * f(x * h, (t - 1) * tau);
+                else
+                    data[t][x] = 0.5 * (data[t-1][x + 1] + data[t-1][x - 1]) - (tau / 2*h) * (data[t-1][x + 1] - data[t-1][x - 1]) + tau * f(x * h, (t - 1) * tau);
             }
             int dest = (t + 1) % commsize;
             MPI_Send(&(data[t][x]), 1, MPI_DOUBLE, dest, x, MPI_COMM_WORLD);
@@ -105,23 +117,37 @@ std::vector<double> do_last_part(std::vector<std::vector<double>>& data, int32_t
         else
             break;
     }
-
     return result;
 }
 
 void do_linear(std::vector<std::vector<double>>& data) {
+
+    auto start = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
+
     std::vector<double> result{};
-    
+
+
     for (uint32_t t = 1; t < t_steps; ++t) {
-        for (uint32_t x = 0; x < x_steps - 1; ++x)
-            data[t][x + 1] =  data[t][x] - (tau / h) * (data[t][x] - data[t-1][x]) + tau * f(x * h, t * tau);
+        for (uint32_t x = 1; x < x_steps; ++x)
+            if (x == x_steps - 1)
+                data[t][x] =  data[t-1][x] - (tau / h) * (data[t-1][x] - data[t-1][x - 1]) + tau * f(x * h, (t - 1) * tau);
+            else
+                data[t][x] = 0.5 * (data[t-1][x + 1] + data[t-1][x - 1]) - (tau / 2*h) * (data[t-1][x + 1] - data[t-1][x - 1]) + tau * f(x * h, (t - 1) * tau);
     }
+
 
     for (int32_t t = 0; t < t_steps; ++t)
         for (int32_t x = 0; x < x_steps; ++x)
             result.push_back(data[t][x]);
 
+    MPI_Barrier(MPI_COMM_WORLD);
+    auto end = MPI_Wtime();
+
     dump(result, std::string{"data_linear.out"});
+
+    std::cout << (end - start) << " " << 1 << std::endl;
+
 }
 
 std::vector<std::vector<double>> init() {
